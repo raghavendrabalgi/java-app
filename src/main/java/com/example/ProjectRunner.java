adaf7ae4975b75339d5b241048513717cd71cda2
raghavendra balgi <raghumbalgi84@gmail.com>
2026-05-19 10:57:53 +0530

    Update ProjectRunner.java

--- src/main/java/com/example/ProjectRunner.java
+++ src/main/java/com/example/ProjectRunner.java
@@ -5,7 +5,7 @@ import com.google.common.base.Strings;
 public class ProjectRunner {
     public static void main(String[] args) {
         String notNullOrEmpty = Strings.nullToEmpty(null);
-        System.out.println("Hello, Bazel! The string is not null or empty: " + notNullOrEmpty);
+        System.out.println("Hello, Bazel! The string is nottesting null or empty: " + notNullOrEmpty);
     }
 }
 
